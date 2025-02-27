0441303d734bf050ce95f67c31d154786e878055
ariasguerra <arias.guerraa@gmail.com>
2025-02-27 17:35:09 -0500

    Delete Prueba directory

--- Prueba/main.js
+++
@@ -1,7 +0,0 @@
-// Punto de entrada principal
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM completamente cargado y analizado');
-    
-    // Iniciar la aplicación
-    AppController.init();
-});
